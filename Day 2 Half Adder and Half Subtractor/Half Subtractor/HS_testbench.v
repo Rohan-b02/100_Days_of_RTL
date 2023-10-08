e86a6432ac82c8b1eee6877c0928692a9b73b3e9
Rohan Boge <146671579+Rohan-b02@users.noreply.github.com>
2023-10-08 22:54:20 +0530

    Update and rename HA_testbench.v to HS_testbench.v

---
+++ Day 2 Half Adder and Half Subtractor/Half Subtractor/HS_testbench.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:44:36 10/08/2023
+// Design Name:   HS
+// Module Name:   HA_testbench.v
+// Project Name:  Half_Subtractor
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: HS
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module HA_testbench_v;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire diff;
+	wire borr;
+
+	// Instantiate the Unit Under Test (UUT)
+	HS uut (
+		.a(a), 
+		.b(b), 
+		.diff(diff), 
+		.borr(borr)
+	);
+
+	initial begin
+		// Initialize Inputs
+		    a = 0;b = 0;
+		#10 a = 0;b = 1;
+		#10 a = 1;b = 0;
+		#10 a = 1;b = 1;
+		
+		#40 $finish;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
