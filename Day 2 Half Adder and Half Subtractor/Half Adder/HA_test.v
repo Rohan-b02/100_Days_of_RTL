86ea8aa11dda6ba913f993de610c542697059eba
Rohan Boge <146671579+Rohan-b02@users.noreply.github.com>
2023-10-08 22:51:55 +0530

    Add files via upload

---
+++ Day 2 Half Adder and Half Subtractor/Half Adder/HA_test.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: VIT
+// Engineer: Rohan
+
+////////////////////////////////////////////////////////////////////////////////
+
+module HA_test_v;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire sum;
+	wire carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	ha uut (
+		.a(a), 
+		.b(b), 
+		.sum(sum), 
+		.carry(carry)
+	);
+
+	initial begin
+		// Initialize Inputs
+			a = 0;b = 0;#10 a = 0;b = 1;
+#10 a = 1;b = 0;
+#10 a = 1;b = 1;
+#40 $finish;
+	end
+      
+endmodule
+
