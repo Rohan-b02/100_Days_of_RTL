8c8307479d4e2f1c013ac1b9c3568159a6f0b610
Rohan Boge <146671579+Rohan-b02@users.noreply.github.com>
2023-10-09 22:50:37 +0530

    Add files via upload

---
+++ Day 3 Full Adder and Full Subtractor/Full Adder/FA_testbench.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: VIT
+// Engineer: Rohan Boge
+// Create Date:   22:29:45 10/09/2023
+// Design Name:   FA
+// Module Name:   FA_testbench.v
+// Project Name:  full_adder
+////////////////////////////////////////////////////////////////////////////////
+
+module FA_testbench_v;
+
+	// Inputs
+	reg a;
+	reg b;
+	reg cin;
+
+	// Outputs
+	wire sum;
+	wire carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	FA uut (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.sum(sum), 
+		.carry(carry)
+	);
+
+	initial begin
+		// Initialize Inputs
+		
+	a = 1'b0; b = 1'b0; cin = 1'b0;
+  #10;
+  a = 1'b0; b = 1'b0; cin = 1'b1;
+  #10;
+  a = 1'b0; b = 1'b1; cin = 1'b0;
+  #10;
+  a = 1'b0; b = 1'b1; cin = 1'b1;
+  #10;
+  a = 1'b1; b = 1'b0; cin = 1'b0;
+  #10;
+  a = 1'b1; b = 1'b0; cin = 1'b1;
+  #10;
+  a = 1'b1; b = 1'b1; cin = 1'b0;
+  #10;
+  a = 1'b1; b = 1'b1; cin = 1'b1;
+  #10;
+  $finish;
+
+	end
+      
+endmodule
+
---
+++ Day 3 Full Adder and Full Subtractor/Full Subtractor/HS_testbench.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company:  VIT
+// Engineer: Rohan Boge
+//
+// Create Date:   22:41:57 10/09/2023
+// Design Name:   full_subtractor
+// Module Name:   HS_testbench.v
+// Project Name:  full_sub 
+////////////////////////////////////////////////////////////////////////////////
+
+module HS_testbench_v;
+
+	// Inputs
+	reg a;
+	reg b;
+	reg bin;
+
+	// Outputs
+	wire diff;
+	wire bout;
+
+	// Instantiate the Unit Under Test (UUT)
+	full_subtractor uut (
+		.a(a), 
+		.b(b), 
+		.bin(bin), 
+		.diff(diff), 
+		.bout(bout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		
+	a = 1'b0; b = 1'b0; bin = 1'b0;
+  #10;
+  a = 1'b0; b = 1'b0; bin = 1'b1;
+  #10;
+  a = 1'b0; b = 1'b1; bin = 1'b0;
+  #10;
+  a = 1'b0; b = 1'b1; bin = 1'b1;
+  #10;
+  a = 1'b1; b = 1'b0; bin = 1'b0;
+  #10;
+  a = 1'b1; b = 1'b0; bin = 1'b1;
+  #10;
+  a = 1'b1; b = 1'b1; bin = 1'b0;
+  #10;
+  a = 1'b1; b = 1'b1; bin = 1'b1;
+	#10;
+	$finish;
+
+	end
+      
+endmodule
+
