04b9d85fa3acd52bfd0015d1b302885e941a8d83
Rohan Boge <146671579+Rohan-b02@users.noreply.github.com>
2023-10-10 23:29:32 +0530

    Add files via upload

---
+++ Day 4 Full Adder using Half adder/FA.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: VIT
+// Engineer: Rohan Boge
+//////////////////////////////////////////////////////////////////////////////////
+module HA(a,b,sum,cout);
+
+    input a,b;
+    output sum,cout;
+
+    assign sum = a^b;
+    assign cout = a&b;
+
+endmodule
+
+
+module FA(a,b,cin,sum,cout);
+
+    input a,b,cin;
+    output sum,cout;
+    wire t1,c1,c2;
+    HA h1(a,b,t1,c1);
+    HA h2(cin,t1,sum,c2);
+
+    assign cout = c1 | c2;
+
+endmodule
---
+++ Day 4 Full Adder using Half adder/FA_test.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:00:19 10/10/2023
+// Design Name:   FA
+// Module Name:   FA_test.v
+// Project Name:  full_adderHA
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FA
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module FA_test_v;
+
+	// Inputs
+	reg a;
+	reg b;
+	reg cin;
+
+	// Outputs
+	wire sum;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	FA uut (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.sum(sum), 
+		.cout(cout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+		cin = 0;
+
+	#10;
+	   a = 0;
+		b = 0;
+		cin = 1;
+	#10;
+	   a = 0;
+		b = 1 ;
+		cin = 0;
+	#10;
+	   a = 0;
+		b = 1;
+		cin = 1;
+	#10;
+	   a = 1;
+		b = 0;
+		cin = 0;
+	#10;
+	   a = 1;
+		b = 0;
+		cin = 1;
+	#10;
+	   a = 1;
+		b = 1;
+		cin = 0;
+	#10;
+	   a = 1;
+		b = 1;
+		cin = 1;
+	#10 $finish;
+		
+
+	end
+      
+endmodule
+
