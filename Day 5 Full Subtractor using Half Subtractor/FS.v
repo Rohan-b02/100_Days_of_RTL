1917f0aff10dda427193c492c20bd6f8b3ceb17a
Rohan Boge <146671579+Rohan-b02@users.noreply.github.com>
2023-10-11 21:55:39 +0530

    Add files via upload

---
+++ Day 5 Full Subtractor using Half Subtractor/FS.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: VIT
+// Engineer: Rohan Boge
+//////////////////////////////////////////////////////////////////////////////////
+module HS(a,b,diff,bout);
+	input a,b;
+	output diff,bout;
+	
+	assign diff = a^b;
+	assign bout =(~a)&b;
+
+endmodule
+
+module FS(a,b,bin,diff,bout);
+	input a,b,bin;
+	output diff,bout;
+	
+	wire d1,b1,b2;
+	
+	HS s1(a,b,d1,b1);
+	HS s2(d1,bin,diff,b2);
+	
+	assign bout = b1|b2;
+	
+endmodule
---
+++ Day 5 Full Subtractor using Half Subtractor/FS_testbench.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: VIT
+// Engineer: Rohan Boge
+// Create Date:   21:37:19 10/11/2023
+// Design Name:   FS
+// Module Name:   FS_testbench.v
+// Project Name:  full_subt
+////////////////////////////////////////////////////////////////////////////////
+
+module FS_testbench_v;
+
+	// Inputs
+	reg a;
+	reg b;
+	reg bin;
+
+	// Outputs
+	wire diff;
+	wire bout;
+
+	// Instantiate the Unit Under Test (UUT)
+	FS uut (
+		.a(a), 
+		.b(b), 
+		.bin(bin), 
+		.diff(diff), 
+		.bout(bout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+		bin = 0;
+
+	#10
+		a = 0;
+		b = 0;
+		bin =1;
+	#10
+		a = 0;
+		b = 1;
+		bin = 0;
+	#10
+		a = 0;
+		b = 1;
+		bin = 1;
+	#10
+		a = 1;
+		b = 0;
+		bin = 0;
+	#10
+		a = 1;
+		b = 0;
+		bin = 1;
+	#10
+		a = 1;
+		b = 1;
+		bin = 0;
+	#10
+		a = 1;
+		b = 1;
+		bin = 1;
+	#10 
+		$finish;
+	end
+      
+endmodule
+
